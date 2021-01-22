6e8eeb69e0135841a585a836f63c4c0615ee247d
James Wyn <thisisjameswyn@gmail.com>
2021-01-22 09:51:17 -0800

    add modified code that pushes numbers to array based on integer place value

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-function howManyDigits(userNumber)  {
-  let howMany = userNumber.length;
-  return howMany;
-}
+//UI logic
+
+
+//Business
+
+function numberedArray(inputNumber) {
+  let numberArray = [];
+    for (let index = 0; index <= inputNumber; index += 1) {
+      numberArray.push(index); 
+    }
+    /* const array = [inputNumber]; */
+    console.log(numberArray);
+  }
+  
+  numberedArray(6);
+
+//split individual digits, add to array in order
+//evaluate each number
+  //{for each - number in array}
+//if
