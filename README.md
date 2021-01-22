e4f6212cbc93c5a2589d304875632537b99d12a2
James Wyn <thisisjameswyn@gmail.com>
2021-01-22 10:26:38 -0800

    add framework to split number into index locations and compare

--- README.md
+++ README.md
@@ -18,3 +18,6 @@ Expect(robogerArray(5).toEqual([0, "Beep!", "Boop!", "Won't you be my neighbor?"
 
 Test: "It should return an array with 0 and "Beep!" and "Boop!" and "Won't you be my neighbor?" and 4, 5...."Beep!" if 10 is inputted"
 Expect(robogerArray(10).toEqual([0, "Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, 6, 7, 8, 9, "Beep!"])
+
+Test: "It should return "Beep!", "Boop!" or "Won't you be my neighbor?" if a 1, 2 or 3 is in part or is the number.
+Expect(robogerArray(3).toEqual(["Won't you be my neighbor?"])
--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,27 @@
 //Business
 
 function numberedArray(inputNumber) {
+  const compArray = [1,2,3];
   let numberArray = [];
     for (let index = 0; index <= inputNumber; index += 1) {
-      numberArray.push(index); 
+      if(compArray.includes(index)) {
+        numberArray.push("Check!");
+      } else {
+        numberArray.push(index);
+      }
+      
+      
     }
     /* const array = [inputNumber]; */
     console.log(numberArray);
   }
   
-  numberedArray(6);
+  numberedArray(20);
+  
 
-//split individual digits, add to array in order
-//evaluate each number
-  //{for each - number in array}
-//if
+//take input number and assign value to index amount
+//add number sequentially to array based on amount of input number
+//check each index in the array for a 1 2 or 3 against a comparison array
+//replace all 1 with "Beep!"
+//replace all 2 with "Boop!"
+//replace all 3 with "Won't you be my neighbor?"
